81d4cde50ca99631d4d57bd7543c759f623594ef
Leandro Landie <landie@hotmail.com>
2023-02-06 22:17:12 -0300

    mejoras

--- js/function.js
+++ js/function.js
@@ -1,1 +1,8 @@
-document.getElementById("button").onclick = document.getElementById("demo").innerHTML = "anda a cagar";
+function mostrarhora(){
+   momentoactual = new Date();
+   hora= momentoactual.getHours();
+   minuto=momentoactual.getMinutes();
+   segundos=momentoactual.getSeconds();
+   horaimprimible= hora+" : "+minuto+" : "+segundo;
+   document.getElementById("mostarhora").innerHTML=horaimprimible
+}
